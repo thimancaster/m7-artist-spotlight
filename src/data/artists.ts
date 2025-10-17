12ae3824645cb9ecb14bb09315d1439137b6e789
Thiago Ferreira <thimancaster@hotmail.com>
2025-10-17 15:17:47 -0300

    confirmar

--- src/data/artists.ts
+++ src/data/artists.ts
@@ -8,6 +8,8 @@ export interface Artist {
     instagram?: string;
     youtube?: string;
     spotify?: string;
+    appleMusic?: string;
+    deezer?: string;
   };
   featuredVideoUrl?: string;
 }
@@ -18,11 +20,13 @@ export const artists: Artist[] = [
     name: "Julliany Souza",
     genre: "Adoração e Pop Cristão",
     bio: "Julliany Souza é uma das principais cantoras e compositoras da música cristã no Brasil. Com uma voz expressiva, consolidou sua carreira como representante da música gospel nacional e internacional, com canções como 'A Casa É Sua' e 'Eu Te Vejo Em Tudo'.",
-    image: "https://www.letras.mus.br/fotos/artista/a/1/7/d/a17d5c5a2c2b3e8b5d3c3a0d5e1f1f1f-tb.jpg",
+    image: "https://yt3.googleusercontent.com/AWQcmMIXDo3NZbANwfp0lvnCuNHbhw_riLdVkSs_fn1hz3Q8-DagO6NU9kb4ngyQGiSrPgPwqQ=s900-c-k-c0x00ffffff-no-rj",
     socialMedia: {
       instagram: "https://www.instagram.com/jullianysouza",
       youtube: "https://www.youtube.com/@jullianysouza",
-      spotify: "https://open.spotify.com/artist/0_1g2s5ztmr3d5qH4tIEeX"
+      spotify: "https://open.spotify.com/artist/111v5kC022h042A48n4JHA",
+      appleMusic: "https://music.apple.com/br/artist/julliany-souza/1541296225",
+      deezer: "https://www.deezer.com/br/artist/113247072"
     },
     featuredVideoUrl: "https://www.youtube.com/watch?v=xcC3Xh3PFcE"
   },
@@ -31,11 +35,13 @@ export const artists: Artist[] = [
     name: "Jhonas Serra",
     genre: "Louvor e Adoração",
     bio: "Jhonas Serra, natural de Brasília, é filho de pastores e iniciou seu ministério cedo. Hoje, representa sua cidade e país com mais de 39 milhões de pessoas alcançadas com músicas como 'Só quero queimar' e 'Furioso Oceano'.",
-    image: "https://www.letras.mus.br/fotos/artista/7/1/c/e/71ce2893f415383f915f75e9f8c6b4e0-tb.jpg",
+    image: "https://yt3.googleusercontent.com/DQ86LPAulZw7llfm00vGWtcX3-rlFTwq7i6mp_xE_RzMhwUpPGln732ToTTrrNEf9cD2wMzv1OY=s900-c-k-c0x00ffffff-no-rj",
     socialMedia: {
       instagram: "https://www.instagram.com/jhonasserra",
       youtube: "https://www.youtube.com/@jhonasserra",
-      spotify: "https://open.spotify.com/artist/0WsTM0BMHTqJM5M7EGX4EH"
+      spotify: "https://open.spotify.com/artist/0WsTM0BMHTqJM5M7EGX4EH",
+      appleMusic: "https://music.apple.com/br/artist/jhonas-serra/1501799748",
+      deezer: "https://www.deezer.com/br/artist/87964722"
     },
     featuredVideoUrl: "https://www.youtube.com/watch?v=2f38h32R_2s"
   },
@@ -44,11 +50,13 @@ export const artists: Artist[] = [
     name: "Theo Rubia",
     genre: "Worship e Adoração",
     bio: "Theo Rubia é um jovem cantor e pastor que se apaixonou pela música na infância. Compositor de canções como 'Pode Morar Aqui' e 'Eu Só Quero Tua Presença', que têm tocado em igrejas por todo o Brasil e outros países.",
-    image: "https://www.letras.mus.br/fotos/artista/c/9/c/d/c9cd0a4a2f8c6e3b5e4d2d4f2c5d1d6a-tb.jpg",
+    image: "https://yt3.googleusercontent.com/3DIIBhph7dL_uyao6jJlUwa4a4GJ4AIlpB8UelDTKC8xfFFslUr5T34A7DgQWFH7RFSn7dPo=s900-c-k-c0x00ffffff-no-rj",
     socialMedia: {
       instagram: "https://www.instagram.com/theorubia",
       youtube: "https://www.youtube.com/@theorubia",
-      spotify: "https://open.spotify.com/artist/3bTnUX1j5AUASk2VzgK851"
+      spotify: "https://open.spotify.com/artist/3bTnUX1yo0SMLzHk2yU1w2",
+      appleMusic: "https://music.apple.com/br/artist/theo-rubia/1273397992",
+      deezer: "https://www.deezer.com/br/artist/13019821"
     },
     featuredVideoUrl: "https://www.youtube.com/watch?v=uUaDScX0PRE"
   },
@@ -57,11 +65,13 @@ export const artists: Artist[] = [
     name: "Karem Cardim",
     genre: "Louvor e Pop Gospel",
     bio: "A Missionária Karem Cardim leva o amor de Deus através de sua delicadeza. Seu ministério tem se destacado no Brasil com mensagens fundamentadas nas escrituras e vídeos que ultrapassam 500 mil visualizações no YouTube.",
-    image: "https://m7producoes.com.br/wp-content/uploads/2022/07/karem-cardim.png",
+    image: "https://yt3.googleusercontent.com/-bUSOutpa1_KAMBrnlAOUt4LcDrgaL3wI-ele52Pnn47YiVns_HoIHdTy1cQ6jIzzlsG8fA4=s900-c-k-c0x00ffffff-no-rj",
     socialMedia: {
       instagram: "https://www.instagram.com/karemcardim",
       youtube: "https://www.youtube.com/@karemcardim",
-      spotify: "https://open.spotify.com/artist/23i32UFe24iJgo1042S5aL"
+      spotify: "https://open.spotify.com/artist/6e1hS3a64k2AKn1gupq0yQ",
+      appleMusic: "https://music.apple.com/br/artist/karem-cardim/1531885455",
+      deezer: "https://www.deezer.com/br/artist/107873432"
     },
     featuredVideoUrl: "https://www.youtube.com/watch?v=kI4D9wVPbOs"
   },
@@ -70,11 +80,13 @@ export const artists: Artist[] = [
     name: "Gabriell Júnior",
     genre: "Música Cristã Contemporânea",
     bio: "Gabriell Júnior, cantor e compositor de Goiás, cresceu em uma família musical e pastoral. Cristão apaixonado pela obra de Deus, já alcançou a marca de mais de 15 milhões de visualizações em seu canal no YouTube.",
-    image: "https://i.ytimg.com/vi/qPFJrP2sszM/maxresdefault.jpg",
+    image: "https://yt3.googleusercontent.com/bt5HXqbItwqhQ_qBkwR09OoyhnraSw_f-yxmmWa9AITWfgTe_FeiGaJ44IRNXoTekH0YsV8soA=s900-c-k-c0x00ffffff-no-rj",
     socialMedia: {
       instagram: "https://www.instagram.com/gabrielljunior",
       youtube: "https://www.youtube.com/@gabrielljunior",
-      spotify: "https://open.spotify.com/artist/3rE6crNoQ1T3aQ1aBf5a7k"
+      spotify: "https://open.spotify.com/artist/5LwG8fX3s1s4wda1a3e6M2",
+      appleMusic: "https://music.apple.com/br/artist/gabriell-júnior/1289637341",
+      deezer: "https://www.deezer.com/br/artist/13352723"
     },
     featuredVideoUrl: "https://www.youtube.com/watch?v=F0f-1j9c_gA"
   },
@@ -83,11 +95,13 @@ export const artists: Artist[] = [
     name: "Danilo Neves",
     genre: "Adoração Jovem",
     bio: "Danilo Neves é um jovem adorador que leva mensagens de fé e esperança. Com presença marcante e milhares de visualizações nas plataformas digitais, cada show é uma experiência de conexão com o público jovem.",
-    image: "https://www.ogol.com.br/img/jogadores/94/246094_med_danilo_neves.jpg",
+    image: "https://yt3.googleusercontent.com/iNLb6-1NNSJTpusT3rs-F6zwIOe3y9hh_HVtI-U8vz4e1boIDymzgzJirF62R80_TVz_h0r2Og=s900-c-k-c0x00ffffff-no-rj",
     socialMedia: {
       instagram: "https://www.instagram.com/danilonevesoficial",
       youtube: "https://www.youtube.com/@danilonevesoficial",
-      spotify: "https://open.spotify.com/artist/11779537"
+      spotify: "https://open.spotify.com/artist/2BZNtCgTr3oEAP22I3dI54",
+      appleMusic: "https://music.apple.com/br/artist/danilo-neves/1737327993",
+      deezer: "https://www.deezer.com/br/artist/11967799"
     },
     featuredVideoUrl: "https://www.youtube.com/watch?v=w9h7j4f-eSg"
   },
@@ -96,11 +110,13 @@ export const artists: Artist[] = [
     name: "Ton Molinari",
     genre: "Worship / Pop Cristão",
     bio: "Ton Molinari é um reconhecido cantor, compositor e adorador da música cristã contemporânea. Suas canções tocam corações e levam mensagens de esperança em eventos por todo o Brasil.",
-    image: "https://media.gettyimages.com/id/1239324508/photo/anthony-molinari-attends-the-premiere-of-20th-century-studios-the-kings-man-at-museum-of-modern.jpg?s=612x612&w=gi&k=20&c=6_-_t5h4k3z-Q8hL-_B4D4E8Y8A4Z8Y4c8Y4Z8Y4c8Y4=",
+    image: "https://yt3.googleusercontent.com/dY7UehHW7CZbAa7I3nOXlQ4oGMBqRJDc6eIrIew7biiVSclCR2M8cWKXYc8pQLQPNhws0tJ5Cw=s900-c-k-c0x00ffffff-no-rj",
     socialMedia: {
       instagram: "https://www.instagram.com/tonmolinari",
       youtube: "https://www.youtube.com/@tonmolinari",
-      spotify: "https://open.spotify.com/artist/11779537"
+      spotify: "https://open.spotify.com/artist/336822y0H3fOP1zG8wGNh4",
+      appleMusic: "https://music.apple.com/br/artist/ton-molinari/1544490812",
+      deezer: "https://www.deezer.com/br/artist/117363402"
     },
     featuredVideoUrl: "https://www.youtube.com/watch?v=1Z-Z1Z1Z1Z1"
   },
@@ -109,11 +125,13 @@ export const artists: Artist[] = [
     name: "Gabriela Gomes",
     genre: "Adoração e Louvor",
     bio: "Filha de Marquinhos Gomes, Gabriela iniciou sua carreira aos 12 anos. Seu álbum de estreia, 'Não vou perder a Fé', e o single 'Deus Proverá' consolidaram seu nome na música cristã nacional.",
-    image: "https://rafavidal.com.br/wp-content/uploads/2021/08/Gabi_1-1-1024x683.jpg",
+    image: "https://yt3.googleusercontent.com/IBk04ozUvk0UkJNPm-aNEMneVaPY-5xL3srrYThr8Fyk9k7QreHZJrHfVNVhMbaZ057WIM22CQ=s900-c-k-c0x00ffffff-no-rj",
     socialMedia: {
       instagram: "https://www.instagram.com/gabrielagomesoficial",
       youtube: "https://www.youtube.com/@gabrielagomesoficial",
-      spotify: "https://open.spotify.com/artist/20212260"
+      spotify: "https://open.spotify.com/artist/2rGIL7iQ6w0I15bbfwwaCw",
+      appleMusic: "https://music.apple.com/br/artist/gabriela-gomes/991754983",
+      deezer: "https://www.deezer.com/br/artist/6009928"
     },
     featuredVideoUrl: "https://www.youtube.com/watch?v=LZflt_pD3wE"
   },
@@ -122,11 +140,13 @@ export const artists: Artist[] = [
     name: "Felipe Vilela",
     genre: "Rap Gospel",
     bio: "Felipe Vilela descobriu seu talento para o RAP aos 11 anos. Através de viagens pelo Brasil e exterior, tem levado sua arte e canção, e atualmente é cantor pela Universal Music, aguardando o lançamento de seu DVD 'Orgânico'.",
-    image: "https://i.ytimg.com/vi/pWhV6z0nBsY/maxresdefault.jpg",
+    image: "https://yt3.googleusercontent.com/eeUb-bZPFKy2_jqCvmYQz4MREwqjqGWFcDeUt2bxE3ffNe25-rCrW418AclG4tHtwFEdmSR4hc0=s900-c-k-c0x00ffffff-no-rj",
     socialMedia: {
       instagram: "https://www.instagram.com/felipevilela",
       youtube: "https://www.youtube.com/@felipevilela",
-      spotify: "https://open.spotify.com/artist/54602232"
+      spotify: "https://open.spotify.com/artist/5qjP2P9wQZOLaB3gQvh9E3",
+      appleMusic: "https://music.apple.com/br/artist/felipe-vilela/1446700683",
+      deezer: "https://www.deezer.com/br/artist/54602232"
     },
     featuredVideoUrl: "https://www.youtube.com/watch?v=9g2z1Sp72_g"
   },
@@ -135,11 +155,13 @@ export const artists: Artist[] = [
     name: "Victin",
     genre: "Rap / Poesia Cristã",
     bio: "Victin mescla rap e poesia cristã, trazendo mensagens profundas de fé. Com estilo único, tem conquistado espaço no rap gospel brasileiro, falando diretamente com a juventude.",
-    image: "https://i.ytimg.com/vi/IXkfoAI3KZU/maxresdefault.jpg",
+    image: "https://yt3.googleusercontent.com/M-sS2MeDsxi8AG8WkfblxDX4wk8Vl04H8Wtd1oeja1pMNTR05rf0O6S3Jg8pJbXAz1EtF0ePlA=s900-c-k-c0x00ffffff-no-rj",
     socialMedia: {
       instagram: "https://www.instagram.com/victinoficial",
       youtube: "https://www.youtube.com/@victinoficial",
-      spotify: "https://open.spotify.com/artist/98606442"
+      spotify: "https://open.spotify.com/artist/3bso1qsC1SYCi4q10h82A3",
+      appleMusic: "https://music.apple.com/br/artist/victin/1521190544",
+      deezer: "https://www.deezer.com/br/artist/98606442"
     },
     featuredVideoUrl: "https://www.youtube.com/watch?v=ElbEKw-I3tM"
   },
@@ -148,11 +170,13 @@ export const artists: Artist[] = [
     name: "Priscila Olly",
     genre: "Louvor e Pop",
     bio: "Priscila Olly, ex-integrante do grupo Kemuel, destacou-se por sua impressionante extensão vocal. Agora em carreira solo, ministra louvores especiais que o Senhor tem trazido ao seu coração, consolidando-se no cenário cristão.",
-    image: "https://i.scdn.co/image/ab6761610000e5eb1d1f0f0f5a7e1f1f1f1f1f1f",
+    image: "https://yt3.googleusercontent.com/JT7c7kLMTkyrortzEeImD_G7Er_R0y6lkF5BayE8tNUbiveQgHVwRqL9HWol6057MtOZxxNKjEc=s900-c-k-c0x00ffffff-no-rj",
     socialMedia: {
       instagram: "https://www.instagram.com/priscilaolly",
       youtube: "https://www.youtube.com/@priscilaolly",
-      spotify: "https://open.spotify.com/artist/9422345"
+      spotify: "https://open.spotify.com/artist/31oM9e1p2g2vYd24Y23Y8E",
+      appleMusic: "https://music.apple.com/br/artist/priscila-olly/1647463991",
+      deezer: "https://www.deezer.com/br/artist/185799307"
     },
     featuredVideoUrl: "https://www.youtube.com/watch?v=G_Mbf4oYc8I"
   },
@@ -161,11 +185,13 @@ export const artists: Artist[] = [
     name: "Carol Braga",
     genre: "Pop Cristão",
     bio: "Carol Braga, cantora e compositora de Brasília, iniciou sua jornada musical aos três anos. Recentemente, lançou o álbum 'Fogo que Consome', composto por cinco faixas, e abençoa vidas com suas canções.",
-    image: "https://i.scdn.co/image/ab6761610000e5eb1d1f0f0f5a7e1f1f1f1f1f1f",
+    image: "https://yt3.googleusercontent.com/M84FQ_eB5gZGYaES4nF4LUg6Jqp3dVK9msXH-RmLuDteFcsCJ6IDhRO1_2T7pp8nCDhOutqW2B8=s900-c-k-c0x00ffffff-no-rj",
     socialMedia: {
       instagram: "https://www.instagram.com/carolbragaoficial",
       youtube: "https://www.youtube.com/@carolbragaoficial",
-      spotify: "https://open.spotify.com/artist/4952671"
+      spotify: "https://open.spotify.com/artist/2QC3q1i19e2r8k3PqtyYoW",
+      appleMusic: "https://music.apple.com/br/artist/carol-braga/1638798610",
+      deezer: "https://www.deezer.com/br/artist/4952671"
     },
     featuredVideoUrl: "https://www.youtube.com/watch?v=video-id-carol-braga"
   },
@@ -174,11 +200,13 @@ export const artists: Artist[] = [
     name: "João Antônio",
     genre: "Adoração e Louvor",
     bio: "João Antônio Martins começou a pregar aos 13 anos, compartilhando uma mensagem bíblica de quebrantamento. Escritor e editor, também leciona em seminários teológicos, formando uma nova geração de ministros.",
-    image: "https://www.al.sp.gov.br/repositorio/deputado/fotos/224.jpg",
+    image: "https://yt3.googleusercontent.com/xfLvE9HaEaYwWLKpgGN42N1-Juo0wgznR3PR0fDfkMS93s0fyoiD2tKYc_DXpKn88GLSbZy9QA=s900-c-k-c0x00ffffff-no-rj",
     socialMedia: {
       instagram: "https://www.instagram.com/joaoantoniooficial",
       youtube: "https://www.youtube.com/@joaoantoniooficial",
-      spotify: "https://open.spotify.com/artist/14299839"
+      spotify: "https://open.spotify.com/artist/2qh5eJcfH5t5iQ2t51p639",
+      appleMusic: "https://music.apple.com/br/artist/joão-antonio/1719767896",
+      deezer: "https://www.deezer.com/br/artist/14299839"
     },
     featuredVideoUrl: "https://www.youtube.com/watch?v=8c_aFmXb_0s"
   }
