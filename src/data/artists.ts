bd537818aa6c05424a50a153205ab4169c0678b0
Thiago Ferreira <thimancaster@hotmail.com>
2025-10-17 10:52:21 -0300

    Refactor artist data by removing fields and updating info
    Removed unused social media fields and updated artist bios and featured video URLs.

--- src/data/artists.ts
+++ src/data/artists.ts
@@ -8,9 +8,6 @@ export interface Artist {
     instagram?: string;
     youtube?: string;
     spotify?: string;
-    appleMusic?: string;
-    deezer?: string;
-    amazonMusic?: string;
   };
   featuredVideoUrl?: string;
 }
@@ -20,16 +17,14 @@ export const artists: Artist[] = [
     id: "julliany-souza",
     name: "Julliany Souza",
     genre: "Adoração e Pop Cristão",
-    bio: "Julliany Souza é uma das principais cantoras e compositoras da música cristã no Brasil. Com uma voz expressiva, consolidou sua carreira como representante da música gospel nacional e internacional, com canções como 'A Casa É Sua', 'Eu Te Vejo Em Tudo' e o sucesso 'Quem É Esse?'.",
+    bio: "Julliany Souza é uma das principais cantoras e compositoras da música cristã no Brasil. Com uma voz expressiva, consolidou sua carreira como representante da música gospel nacional e internacional, com canções como 'A Casa É Sua' e 'Eu Te Vejo Em Tudo'.",
     image: "http://googleusercontent.com/image_collection/image_retrieval/13516458409624661473_0",
     socialMedia: {
       instagram: "https://instagram.com/jullianysouza",
       youtube: "https://youtube.com/@jullianysouza",
-      spotify: "https://open.spotify.com/artist/0dS84B6JspZf5a9D_22n4s",
-      appleMusic: "https://music.apple.com/artist/julliany-souza/309280301",
-      deezer: "https://www.deezer.com/artist/4564817"
+      spotify: "https://open.spotify.com/artist/0dS84B6JspZf5a9D_22n4s"
     },
-    featuredVideoUrl: "https://www.youtube.com/watch?v=0ZF5em0MTwY"
+    featuredVideoUrl: "https://www.youtube.com/watch?v=xcC3Xh3PFcE"
   },
   {
     id: "jhonas-serra",
@@ -39,10 +34,8 @@ export const artists: Artist[] = [
     image: "http://googleusercontent.com/image_collection/image_retrieval/13802784979550049627_0",
     socialMedia: {
       instagram: "https://instagram.com/jhonasserra",
-      youtube: "https://youtube.com/@JhonasSerra",
-      spotify: "https://open.spotify.com/artist/1hLd1f2o1M1b4Y6vWb2yvN",
-      appleMusic: "https://music.apple.com/us/artist/jhonas-serra/1163308498",
-      deezer: "https://www.deezer.com/artist/10776699"
+      youtube: "https://youtube.com/@jhonasserra",
+      spotify: "https://open.spotify.com/artist/1hLd1f2o1M1b4Y6vWb2yvN"
     },
     featuredVideoUrl: "https://www.youtube.com/watch?v=2f38h32R_2s"
   },
@@ -50,14 +43,12 @@ export const artists: Artist[] = [
     id: "theo-rubia",
     name: "Theo Rubia",
     genre: "Worship e Adoração",
-    bio: "Theo Rubia é um jovem cantor e pastor que se apaixonou pela música na infância. Com mais de 500 milhões de streams, é compositor de canções como 'Pode Morar Aqui' e 'Eu Só Quero Tua Presença', que têm tocado corações em igrejas por todo o Brasil e outros países.",
+    bio: "Theo Rubia é um jovem cantor e pastor que se apaixonou pela música na infância. Compositor de canções como 'Pode Morar Aqui' e 'Eu Só Quero Tua Presença', que têm tocado em igrejas por todo o Brasil e outros países.",
     image: "http://googleusercontent.com/image_collection/image_retrieval/4053657985273739699_0",
     socialMedia: {
       instagram: "https://instagram.com/theorubia",
       youtube: "https://youtube.com/@theorubia",
-      spotify: "https://open.spotify.com/artist/3bTnSZoJC36oNVr0I6AdoA",
-      appleMusic: "https://music.apple.com/artist/theo-rubia/885884265",
-      deezer: "https://www.deezer.com/artist/8688379"
+      spotify: "https://open.spotify.com/artist/3bTnSZoJC36oNVr0I6AdoA"
     },
     featuredVideoUrl: "https://www.youtube.com/watch?v=uUaDScX0PRE"
   },
@@ -65,31 +56,27 @@ export const artists: Artist[] = [
     id: "karem-cardim",
     name: "Karem Cardim",
     genre: "Louvor e Pop Gospel",
-    bio: "A Missionária Karem Cardim nasceu em Ceilândia-DF e leva o amor de Deus através de sua delicadeza. Seu ministério tem se destacado no Brasil com mensagens fundamentadas nas escrituras, com sucessos como 'Tempestades Passam' e 'Lições de Ana', ultrapassando milhões de visualizações no YouTube.",
+    bio: "A Missionária Karem Cardim leva o amor de Deus através de sua delicadeza. Seu ministério tem se destacado no Brasil com mensagens fundamentadas nas escrituras e vídeos que ultrapassam 500 mil visualizações no YouTube.",
     image: "http://googleusercontent.com/image_collection/image_retrieval/7638537924895246572_0",
     socialMedia: {
       instagram: "https://instagram.com/karemcardim",
       youtube: "https://youtube.com/@karemcardim",
-      spotify: "https://open.spotify.com/artist/62fcY92o21yS0g3S2g3s14",
-      appleMusic: "https://music.apple.com/artist/karem-cardim/1445566281",
-      deezer: "https://www.deezer.com/artist/60685932"
+      spotify: "https://open.spotify.com/artist/62fcY92o21yS0g3S2g3s14"
     },
-    featuredVideoUrl: "https://www.youtube.com/watch?v=9Wb5nC0W1Vs"
+    featuredVideoUrl: "https://www.youtube.com/watch?v=kI4D9wVPbOs"
   },
   {
     id: "gabriell-junior",
     name: "Gabriell Júnior",
     genre: "Música Cristã Contemporânea",
-    bio: "Gabriell Júnior, com 25 anos, é cantor e compositor de Goiás, casado com Gabrielly Reis e pai da pequena Bela. Cresceu em uma família musical e pastoral, apaixonado pela obra de Deus. Com sucessos como 'Amo o Senhor' e 'Amar Como Você', já alcançou mais de 18 milhões de visualizações no YouTube.",
+    bio: "Gabriell Júnior, cantor e compositor de Goiás, cresceu em uma família musical e pastoral. Cristão apaixonado pela obra de Deus, já alcançou a marca de mais de 15 milhões de visualizações em seu canal no YouTube.",
     image: "http://googleusercontent.com/image_collection/image_retrieval/4335866239597984081_0",
     socialMedia: {
       instagram: "https://instagram.com/gabrielljunior",
-      youtube: "https://youtube.com/@GabriellJunior",
-      spotify: "https://open.spotify.com/artist/5dK2p5C62aNh51a4fXSD2E",
-      appleMusic: "https://music.apple.com/artist/gabriell-junior/1444835551",
-      deezer: "https://www.deezer.com/artist/60440062"
+      youtube: "https://youtube.com/@gabrielljunior",
+      spotify: "https://open.spotify.com/artist/5dK2p5C62aNh51a4fXSD2E"
     },
-    featuredVideoUrl: "https://www.youtube.com/watch?v=TC2NqwT8x_M"
+    featuredVideoUrl: "https://www.youtube.com/watch?v=F0f-1j9c_gA"
   },
   {
     id: "danilo-neves",
@@ -119,14 +106,12 @@ export const artists: Artist[] = [
     id: "gabriela-gomes",
     name: "Gabriela Gomes",
     genre: "Adoração e Louvor",
-    bio: "Filha de Marquinhos Gomes, Gabriela iniciou sua carreira aos 12 anos. Artista da Universal Music, seu álbum 'Não vou perder a Fé' e sucessos como 'Deus Proverá', 'A Melhor Parte' e 'É Sobre Ele' consolidaram seu nome na música cristã nacional.",
+    bio: "Filha de Marquinhos Gomes, Gabriela iniciou sua carreira aos 12 anos. Seu álbum de estreia, 'Não vou perder a Fé', e o single 'Deus Proverá' consolidaram seu nome na música cristã nacional.",
     image: "http://googleusercontent.com/image_collection/image_retrieval/1867464452502639303_0",
     socialMedia: {
       instagram: "https://instagram.com/gabrielagomesoficial",
       youtube: "https://youtube.com/@gabrielagomesoficial",
-      spotify: "https://open.spotify.com/artist/2e6fPpl1pABpCA3pG6w3eA",
-      appleMusic: "https://music.apple.com/artist/gabriela-gomes/940165491",
-      deezer: "https://www.deezer.com/artist/6009928"
+      spotify: "https://open.spotify.com/artist/2e6fPpl1pABpCA3pG6w3eA"
     },
     featuredVideoUrl: "https://www.youtube.com/watch?v=LZflt_pD3wE"
   },
@@ -134,22 +119,23 @@ export const artists: Artist[] = [
     id: "felipe-vilela",
     name: "Felipe Vilela",
     genre: "Rap Gospel",
-    bio: "Felipe Vilela descobriu seu talento para o RAP aos 11 anos. Pastor no Ministério MEVAM e artista da Universal Music, soma mais de 18 milhões de visualizações no YouTube e 188 mil ouvintes mensais no Spotify. É destaque no rap cristão brasileiro com sucessos como 'Essa É A Hora'.",
+    bio: "Felipe Vilela descobriu seu talento para o RAP aos 11 anos. Através de viagens pelo Brasil e exterior, tem levado sua arte e canção, e atualmente é cantor pela Universal Music, aguardando o lançamento de seu DVD 'Orgânico'.",
     image: "http://googleusercontent.com/image_collection/image_retrieval/13265581135659596282_0",
     socialMedia: {
-      instagram: "https://instagram.com/felipevilela5pl",
-      youtube: "https://youtube.com/@felipevilela",
-      spotify: "https://open.spotify.com/artist/3yY2gUcIsjMr8hjo2AXk0r",
-      appleMusic: "https://music.apple.com/artist/felipe-vilela/1440879989"
+      instagram: "https://instagram.com/felipevilela",
+      youtube: "https://youtube.com/@felipevilela"
     },
-    featuredVideoUrl: "https://www.youtube.com/watch?v=vwrM4KJKhyI"
+    featuredVideoUrl: "https://www.youtube.com/watch?v=9g2z1Sp72_g"
   },
   {
     id: "victin",
     name: "Victin",
     genre: "Rap / Poesia Cristã",
     bio: "Victin mescla rap e poesia cristã, trazendo mensagens profundas de fé. Com estilo único, tem conquistado espaço no rap gospel brasileiro, falando diretamente com a juventude.",
-    image: "http://googleusercontent.com/image_collection/image_retrieval/7500056956931783368_0",
+    image: "
+
+http://googleusercontent.com/image_collection/image_retrieval/7500056956931783368_0
+",
     socialMedia: {
       instagram: "https://instagram.com/victinoficial",
       youtube: "https://youtube.com/@victinoficial"
